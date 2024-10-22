26b75f1c3f13bf681160552662e1a733c1a003bc
Allison <Allison.Boone@keyin.com>
2024-10-22 20:11:27 -0230

    Added code to Periodical.java

--- src/library/items/Periodical.java
+++ src/library/items/Periodical.java
@@ -1,5 +1,31 @@
 package library.items;
 
-public class Periodical {
+// Created the Periodical class.
+public class Periodical extends LibraryItem{
+    private String type;
+
+    public Periodical(String id, String title, String author, String isbn, String publisher, int copies, String type){
+        super(id, title, author, isbn, publisher, copies);
+        this.type = type;
+    }
+
+    // Created abstract method.
+    public String getItemType(){
+        return "Periodical - " + type;
+    }
+
+    // Created getter and setter methods.
+    public String getType(){
+        return type;
+    }
+
+    public void setType(String type){
+        this.type = type;
+    }
+
+    // Created toString method.
+    public String toString(){
+        return super.toString() + ", type='" + type + "'";
+    }
     
 }
