2d348dd2f1f355be23f18707cf26d43fd86d975c
Allison <Allison.Boone@keyin.com>
2024-10-22 20:46:11 -0230

    Added code to Employee.java

--- src/library/patrons/Employee.java
+++ src/library/patrons/Employee.java
@@ -1,5 +1,48 @@
 package library.patrons;
 
-public class Employee {
-    
+// Created the Employee class.
+public class Employee extends Patron{
+    private String employeeId;
+    private String department;
+
+    // Created the constructor.
+    public Employee(String name, String address, String phoneNumber, String employeeId, String department){
+        super(name, address, phoneNumber);
+        this.employeeId = employeeId;
+        this.department = department;
+    }
+
+    // Created abstract method for patron type.
+    public String getPatronType(){
+        return "Employee";
+    }
+
+    // Created getter and setter methods.
+    public String getEmployeeId(){
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId){
+        this.employeeId = employeeId;
+    }
+
+    public String getDepartment(){
+        return department;
+    }
+
+    public void setDepartment(String department){
+        this.department = department;
+    }
+
+    // Created toSting method.
+    public String toString() {
+        return "Employee{" +
+                "name='" + getName() + '\'' +
+                ", address='" + getAddress() + '\'' +
+                ", phoneNumber='" + getPhoneNumber() + '\'' +
+                ", employeeId='" + employeeId + '\'' +
+                ", department='" + department + '\'' +
+                ", borrowedItems=" + getBorrowedItems() +
+                '}';
+    }
 }
