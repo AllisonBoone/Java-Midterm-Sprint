4c147f117a5954dc986dd554e5822eeb48e56f66
Allison <Allison.Boone@keyin.com>
2024-10-22 22:38:15 -0230

    Completed the Main.java

--- src/library/Main.java
+++ src/library/Main.java
@@ -1,4 +1,11 @@
 package library;
+// Created Main class and main method to initialize Library and LibraryMenu.
 public class Main {
+    public static void main(String[] args){
+        Library library = new Library();
+        LibraryMenu menu = new LibraryMenu(library);
+
+        menu.startMenu();
+    }
     
 }
